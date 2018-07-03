83aefbb685544da0c9c3c4a7b1fbb84ed78728b2
Mohammed Hamza <medomohammed850@gmail.com>
2018-07-03 20:03:48 +0200

    added js feat.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(function () {
     //start home
+
     $(".home").height($(window).height() - 100);
+
+    //add class active to nav-items
+    $('.nav-item').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
     //end home
 });
