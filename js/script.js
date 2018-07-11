847523ddf9f4e2a15cd59ab3a7ffa161f6fdee09
Mohammed Hamza <medomohammed850@gmail.com>
2018-07-11 14:07:45 +0200

    Added about section part one.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(function () {
-    //start home
+    //Start general
+    //start smooth scroll
+    $('.nav-item .nav-link, .navbar-brand').click(function () {
+        $('html, body').animate({
+            scrollTop : $('#' + $(this).data('target')).offset().top
+        }, 1200);
+    });
+    //end smooth scroll
+    //End general
 
+
+    //start home
     $(".home").height($(window).height() - 100);
 
     //add class active to nav-items
     $('.nav-item').on('click', function () {
         $(this).addClass('active').siblings().removeClass('active');
     });
-
     //end home
 });
