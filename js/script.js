bf92ceb32e6033dd8fbb29311e4b13c96fde28e7
Mohammed Hamza <medomohammed850@gmail.com>
2018-07-11 15:01:15 +0200

    Added to top arrow.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 $(function () {
     //Start general
     //start smooth scroll
-    $('.nav-item .nav-link, .navbar-brand').click(function () {
+    $('.nav-item .nav-link, .navbar-brand, .to-top a').on('click', function () {
         $('html, body').animate({
             scrollTop : $('#' + $(this).data('target')).offset().top
         }, 1200);
     });
     //end smooth scroll
+
+    //start to top arrow
+        $(window).on('scroll',function () {
+            if($(window).scrollTop() >= 400){
+                $('.to-top').fadeIn(500);
+            }else {
+                $('.to-top').fadeOut(200);
+            }
+        });
+    //end to top arrow
     //End general
 
 
